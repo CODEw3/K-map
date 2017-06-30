eb29bf14b2354b9eab6e19ffdf75ef8514223424
ben91198 <ben91198@gmail.com>
2017-06-30 18:53:26 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 /*  main file for program 
     REMINDER:
     please use the tab/space key for indentation (optional) at all times
-    Please set tab width to 4/8
     provide documentation wherever required
     DELETE THIS ONCE YOU'VE READ IT
     */
