a16d6561fd3d68a3f87217a2efe862579b3421ce
ben91198 <ben91198@gmail.com>
2017-06-30 18:43:00 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+/*  main file for program 
+    please use the tab/space key for indentation (optional) at all times
+    Please set tab width to 4/8
+    */
+    #include<iostream>
+    using namespace std;
+    int main()
+    {
+             
+        return 0;
+    }
